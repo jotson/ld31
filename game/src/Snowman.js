58231fb5ddfcc024ad3eb867326d670c6c529e96
John Watson <john@watson-net.com>
2014-12-06 09:38:41 -0800

    Don't allow snowmen to collide up

--- game/src/Snowman.js
+++ game/src/Snowman.js
@@ -1,12 +1,16 @@
 var Snowman = function(x, y) {
     Phaser.Sprite.call(this, game, x, y, 'sprites', 'snowman-00.png');
 
+    this.width = 48;
+    this.height = 48;
     this.anchor.setTo(0.5, 0.5);
-
+    
     game.physics.enable(this, Phaser.Physics.ARCADE);
     this.body.mass = G.snowmanMass;
     this.body.drag.setTo(G.snowmanDrag, 0);
     this.body.maxVelocity.setTo(G.snowmanMaxSpeed, G.snowmanMaxSpeed * 10);
+    this.body.collideWorldBounds = true;
+    this.body.checkCollision.up = false;
     // this.animations.add('default', [0,1,2], 10, true);
     // this.animations.play('default');
 };
